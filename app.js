6c0a0626306022c174a924a8da60aea65fe3f0ec
rishabh <rishabh7890@gmail.com>
2019-09-18 21:29:35 -0400

    mongo connection and def items add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const moment = require("moment");
 const app = express();
+const mongoose = require("mongoose");
 let PORT = process.env.PORT || 3000;
 
 // set up middleware
@@ -14,18 +15,53 @@ app.use(express.static("public"));
 // tell app to use ejs.
 app.set("view engine", "ejs");
 
-// create arrays to be added to later in post requests
-var newItems = [];
-var workItems = [];
+// create new mongo db and connection. // set useNewUrlParser to true to get rid of deprication warning
+mongoose.connect("mongodb://localhost:27017/tdlDB", { useNewUrlParser: true });
 
-// create var for day using momentjs
-var day = moment().format('dddd MMMM Do YYYY');
+// create schema for items in db
+const itemsSchema = new mongoose.Schema ({
+  name: String
+});
+
+// create model for db
+const Item = mongoose.model("Item", itemsSchema);
+
+// create a default document to be entered in Items collection
+const defItem = new Item ({
+  name: "I had an awesome day today 😁"
+});
+
+// put default item 'defItem' into array
+const defItems = [defItem];
+
+// insert defItems array into db using insertMany()
+// Item.insertMany(defItems, function(err){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log("inserted default items to db!");
+//   }
+// });
+
+// create const for day using momentjs
+const day = moment().format('dddd MMMM Do YYYY');
 
 // get method. Be sure to create a views folder so we can use ejs
 app.get("/", function(req, res) {
 
-  // add each new created ejs marker to original render. On posts we just need to redirect back to home route.
-  res.render("list", { listTitle: day, newListItems: newItems });
+  // use find() on model to retrieve items from db
+  Item.find( {}, function(err, results){
+    if(err){
+      console.log(err);
+    } else {
+      // put res.render within find()
+      // correlate listTitle with day const
+      // correlate newListItems with the results we got back from our db
+      res.render("list", { listTitle: day, newListItems: results });
+    }
+  });
+
+  // correlate listTitle with day
 });
 
 app.get("/work", function(req, res) {
@@ -36,12 +72,12 @@ app.get("/work", function(req, res) {
 // post route to add new item to list
 app.post("/", function(req, res){
 
-  var newItem = req.body.newItem;
+  const newItem = req.body.newItem;
   console.log(newItem);
   // console log body of request to see what is getting back when we hit submit button and base if statement off of that
   console.log(req.body);
 
-  // use dddd part of moment because the value will only be the characters before a space, not whole day variable
+  // use dddd part of moment because the value will only be the characters before a space, not whole day const
   if(req.body.add === moment().format('dddd')){
     // push newItem to newItems array
     newItems.push(newItem);
